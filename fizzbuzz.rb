7b90b348f6c552e3dd63ca6ae46634d33e416155
Jonathan T Dean <jonathantomdean@gmail.com>
2020-01-12 03:44:22 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -5,9 +5,9 @@ def fizzbuzz(int)
     if int % 3 == 0
       return "Fizz"
     elsif int % 5 == 0
-      return "buzz"
+      return "Buzz"
     elsif int % 3 == 0 && int % 5 == 0
-      return "fizzbuzz"
+      return "FizzBuzz"
     else
       return nil
     end
